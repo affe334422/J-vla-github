e1cc869dfbc8651e7b24c5f46fc7e6f6f42f8cdf
affe334422 <alle07002@utb.vaxjo.se>
2024-10-07 10:53:47 +0200

    fixat
    jag är braaaaaa

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+static double CirkelOmkrets(double a){
+    double fan = a * 2 * 3.14;
+    return fan;
+}
+
+static double CirkelArea(double b){
+    double piss = 3.14 * b * b;
+    return piss;
+}
+
+double radie=2.4;
+Console.WriteLine("Cirkelns omkrets: "+ CirkelOmkrets(radie));
+Console.WriteLine("Cirkelns area: "+ CirkelArea(radie));
